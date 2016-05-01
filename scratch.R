3f99cd202125f9fe3379a6865470a356faab6389
Phil Allen <phil.allen.gm@gmail.com>
2016-05-01 17:07:44 +0100

    save memory

--- scratch.R
+++ scratch.R
@@ -6,10 +6,3 @@ load("data/pp.64.RData")
 # load("data/pp.8.RData")
 # load("data/pp.1.RData")
 
-ggplot(feat3df, aes(x=normFreq, group=corpus, colour=corpus)) +
-  geom_density() + 
-  scale_x_continuous(trans=reverselog_trans(10)) +
-  # scale_x_log10() +
-  scale_colour_manual(values=cbbPalette) +
-  xlab("Frequency of trigram") + 
-  ggtitle("Density of trigram frequency by corpus")

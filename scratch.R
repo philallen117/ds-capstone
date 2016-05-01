979f9a5a26640022dd5d5b7bd1400f464323eb43
Phil Allen <phil.allen.gm@gmail.com>
2016-05-01 17:37:45 +0100

    splitfiles

--- scratch.R
+++ scratch.R
@@ -2,7 +2,15 @@
 # setwd!
 # definitions from chunks in explore.Rmd
 
-load("data/pp.64.RData")
+# load("data/pp.64.RData")
 # load("data/pp.8.RData")
 # load("data/pp.1.RData")
+
+splitfile <- function(rate) {
+  inname <- paste("data/pp", rate, "RData", sep=".")
+  load(inname)
+  names <- c("v", "dfm1", "dfm2", "dfm3")
+  outname <- function(n) paste("data/pp", rate, n, "RData", sep=".")
+  for(n in names) save(list=n, file=outname(n))
+}
 

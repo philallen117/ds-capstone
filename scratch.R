18615d248436ad65ad48f96fbda855ff3f8b7f73
Phil Allen <phil.allen.gm@gmail.com>
2016-05-01 15:53:07 +0100

    proof-read on 1/8

--- scratch.R
+++ scratch.R
@@ -1,43 +1,15 @@
 # scratch.R
 # setwd!
+# definitions from chunks in explore.Rmd
 
-library(parallel)
-options(mc.cores = 4)
-library(wordcloud)
-library(quanteda)
-library(sm)
+load("data/pp.64.RData")
+# load("data/pp.8.RData")
+# load("data/pp.1.RData")
 
-load("data/pp.8.RData") # move to 1 in report
-
-lenChars <- lapply(v.8, function(v){sort(nchar(v), decreasing=TRUE)})
-lenWords <- lapply(dfm1, function(d){sort(rowSums(d), decreasing=TRUE)})
-wordFreq <- lapply(dfm1, function(d){sort(colSums(d), decreasing=TRUE)})
-gram2Freq <- lapply(dfm2, function(d){sort(colSums(d), decreasing=TRUE)})
-gram3Freq <- lapply(dfm3, function(d){sort(colSums(d), decreasing=TRUE)})
-
-flatCorps <- function(l) {
-  twitter <- data.frame(length=l[["twitter"]], corpus="twitter")
-  blogs <- data.frame(length=l[["blogs"]], corpus="blogs")
-  news <- data.frame(length=l[["news"]], corpus="news")
-  rbind(twitter, blogs, news)
-}
-lenCharsFlat <- flatCorps(lenChars)
-
-# figures ...
-
-# plot densities 
-sm.density.compare(lenCharsFlat$length, lenCharsFlat$corpus, xlab="Length of document (characters)")
-title(main="Density of document length (characters) for corpora")
-# create value labels 
-# add legend via mouse click
-colfill<-c(2:(2+length(levels(cyl.f)))) 
-legend(locator(1), levels(cyl.f), fill=colfill)
-
-
-hist(lenWords[["twitter"]])
-plot(density(lenWords[["twitter"]]))
-
-# library(ngram)
-# library(languageR)
-# library(zipfR)
-# library(markovchain)
+ggplot(feat3df, aes(x=normFreq, group=corpus, colour=corpus)) +
+  geom_density() + 
+  scale_x_continuous(trans=reverselog_trans(10)) +
+  # scale_x_log10() +
+  scale_colour_manual(values=cbbPalette) +
+  xlab("Frequency of trigram") + 
+  ggtitle("Density of trigram frequency by corpus")

f7534306a608bcbe2d779a5dad958c3ff9f84f8a
Phil Allen <phil.allen.gm@gmail.com>
2016-05-06 20:12:56 +0100

    scratch for refactor

--- scratch.R
+++ scratch.R
@@ -1,103 +1,84 @@
-# scratch.R
-# setwd!
-# definitions from chunks in explore.Rmd
-
+library(data.table)
+library(dplyr)
 library(stringi)
-library(tm)
-source("util.R")
-setJavaHeapSizeGB(8)
-library(RWeka)
-library(openNLP)
 
-# other guy used tau for tokenisation, mostly because it does counting of ngrams.
-# that will be ok when have got the text as clean batch of sentences, via opennlp perhaps
+# Making table incrementally ...
+# Could store table when frequencies but before probs, and do probs after
+# Or combine full table on basis of weighted average probs
+# either way need to add data.tables
+# dmodel <- readRDS("data/darragh.rds")
+# smodel <- readRDS("data/m.100.train.sentences.rds")
+# d <- dmodel$nGramModel
+# s <- smodel$nGramModel
+
+# Could I do this without table and dataframe .. data.table(phrase=tokenizedCorpus)
+# Need some simple test data to compare outputs
+# returns table phrase, frequency, word, context
+AddWordAndContext <- function (tokenizedCorpus) {
+  frequencyFrame <- data.frame(table(tokenizedCorpus))
+  frequencyFrame$tokenizedCorpus  <- as.character(frequencyFrame$tokenizedCorpus)
+  frequencyDataTable  <- data.table(frequencyFrame)    
+  setnames(frequencyDataTable, c("phrase", "frequency"))
+  setkey(frequencyDataTable, phrase)
+  rm(frequencyFrame)
+  frequencyDataTable[ , word := GetLastWordInPhrase(as.character(phrase)), by = phrase]
+  frequencyDataTable[ , context := RemoveLastWord(as.character(phrase)), by = phrase]
+  frequencyDataTable
+}
+
+# Try using dplyr chaining to avoid holding on to memory.
+CreateTermProbabilityTableFromCorpus  <- function(tokenizedCorpus, reduceUniGramProbabilities, numberOfResultsByPhrase){ 
+  # Add additional columns for the target word and context
+  tokenizerFrequency  <- AddWordAndContext(tokenizedCorpus)
+  # phrase, word, context, frequency
+  contexts <- group_by(tokenizerFrequency, context, word, phrase) 
+  tokenizerFrequency  <- summarise(contexts, contextCount=sum(frequency))
+  setnames(tokenizerFrequency, c("context", "contextCount"))
+  # Encoding(tokenizerFrequency$word)  <- c("unknown")
+  # Encoding(tokenizerFrequency$phrase)  <- c("unknown")
+  # Encoding(tokenizerFrequency$context)  <- c("unknown")
+  Encoding(tokenizerFrequency$context)  <- c("unknown")
+  setkey(tokenizerFrequency, context)
+  tokenizerFrequency [tokenizerFrequency, probability := frequency/contextCount]
+  # Add additional columns for the n-gram value
+  tokenizerFrequency  <- AddNgramValue(tokenizerFrequency)
+  
+if (reduceUniGramProbabilities){
+  # Lower the probabilities of the unigrams for those occurring multiple times in the same bigram.
+  tokenizerFrequency  <- LowerSpecificUnigramProbabilities(tokenizerFrequency)
+}
+# Cleanup memory.
+gc()
+setkey(tokenizerFrequency, context)
+tokenizerFrequency  <- AddRankForEachContext(tokenizerFrequency)
+tokenizerFrequencySortedRankedAndLimited  <- tokenizerFrequency[rank <= numberOfResultsByPhrase]
+return(tokenizerFrequencySortedRankedAndLimited)
+}
+
+# Leave these alone but get rid and replace with split
+GetLastWordInPhrase  <- function(phrase){
+  # Extracts the last word in a sequence of words, specified as a phrase.
+  # Returns:
+  #   A character vector of one word.
+  stopifnot (is.character (phrase))
+  stopifnot (length (phrase) == 1)
+  words <- SplitPhraseIntoWords(phrase)
+  return(words [length (words)])
+}
+SplitPhraseIntoWords  <- function(phrase){
+  stopifnot (is.character (phrase))
+  words <- unlist (strsplit (phrase, split = "[ ]+"))
+  return(words [nchar (words) > 0])
+}
+RemoveLastWord  <- function(phrase){
+  stopifnot (is.character (phrase))
+  stopifnot (length (phrase) == 1)
+  words <- SplitPhraseIntoWords (phrase)
+  wordsWithoutLastWord <- words [1:length (words)-1]
+  phraseWithoutLastWord  <- paste(wordsWithoutLastWord, collapse = " ")
+  return(phraseWithoutLastWord)
+}
 
 # he also removed all single 3grams, which can be justified in terms of back-off
 # relationship of back-off and smoothing ??? 
-
-
-pc.news <- PCorpus(vc.news.sc, dbControl = list(dbName = "news.raw.64.db", dbType = "DB1"))
-# want to add meta-data on way in. here is one way.
-Transcript <- data.frame(Words = Transcript, Speaker = NA, stringsAsFactors = FALSE)
-Transcript$Speaker[regexpr("LEHRER: ", Transcript$Words) != -1] <- 1
-Transcript$Speaker[regexpr("OBAMA: ", Transcript$Words) != -1] <- 2
-Transcript$Speaker[regexpr("ROMNEY: ", Transcript$Words) != -1] <- 3
-Transcript$Speaker <- na.locf(Transcript$Speaker)
-myCorpus <- Corpus(DataframeSource(Transcript))
-
-
-# sentence markers: diolch and nhadiau don't come up, strangely.
-# so sed those in for .<spc> and ?<spc> and then removepunc?
-# so then i would have a corpus of sentences.
-# can i do that within tm? just use NLP english model? http://stackoverflow.com/questions/18712878/r-break-corpus-into-sentences
-
-# then, really want to find a better way to sample from corpus and keep a 40% hold-out.
-# unless i reverse the sampling stuff I did on the raw file.
-
-vc.blogs.64 <- tm_map(vc.blogs.64, removePunctuation)
-vc.blogs.64 <- tm_map(vc.blogs.64, removeNumbers)
-vc.blogs.64 <- tm_map(vc.blogs.64, stripWhitespace)
-# vc64.blogs[[1]]$content
-
-# Sets the default number of threads to use
-options(mc.cores=2)
-dtm.blogs.64 <- DocumentTermMatrix(vc.blogs.64, control=list(tokenize = WordTokenizer)) 
-
-# maybe work on crude first?
-
-data("crude")
-TrigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3))
-tdmc <- TermDocumentMatrix(crude, control = list(tokenize = TrigramTokenizer))
-tdmc <- TermDocumentMatrix(crude, control = list(tokenize = NGramTokenizer, 3))
-inspect(tdmc[340:345,1:10])
-findFreqTerms(tdmc, lowfreq = 5)[1:10]
-
-plot(tdm, terms = findFreqTerms(tdm, lowfreq = 2)[1:50], corThreshold = 0.5)
-
-f <- system.file("texts", "rcv1_2330.xml", package="tm")
-rcv1 <- readRCV1asPlain(elem = list(content = readLines(f)), language = "en", id = "id1")
-meta(rcv1)
-
-convert_text_to_sentences <- function(text, lang = "en_US") {
-  # Function to compute sentence annotations using the Apache OpenNLP Maxent
-  # sentence detector employing the default model for language 'en'.
-  # or try the one from qdap
-  sentence_token_annotator <- Simple_Sent_Token_Annotator(language = lang)
-  # Convert text to class String from package NLP
-  text <- as.String(text)
-  # Sentence boundaries in text
-  sentence.boundaries <- annotate(text, sentence_token_annotator)
-  # Extract sentences
-  sentences <- text[sentence.boundaries]
-  return(sentences)
-}
-
-reshape_corpus <- function(current.corpus, FUN, ...) {
-  # Extract the text from each document in the corpus and put into a list
-  # text <- lapply(current.corpus, Content)
-  # replace Content with content_transformer
-  # Basically convert the text
-  docs <- lapply(current.corpus, FUN, ...)
-  docs <- as.vector(unlist(docs))
-  # Create a new corpus structure and return it
-  new.corpus <- Corpus(VectorSource(docs))
-  return(new.corpus)
-}
-# Am i better off with dataframe or vectorsource to save it to PCorpus?
-
-inspect(reshape_corpus(crude, convert_text_to_sentences))
-
-library("NLP")
-## Some text.
-s <- paste(c("Pierre Vinken, 61 years old, will join the board as a ",
-             "nonexecutive director Nov. 29.\n",
-             "Mr. Vinken is chairman of Elsevier N.V., ",
-             "the Dutch publishing group."),
-           collapse = "")
-s <- as.String(s)
-sent_token_annotator <- Maxent_Sent_Token_Annotator()
-sent_token_annotator
-a1 <- annotate(s, sent_token_annotator)
-a1
-## Extract sentences.
-s[a1]
+# pc.news <- PCorpus(vc.news.sc, dbControl = list(dbName = "news.raw.64.db", dbType = "DB1"))
